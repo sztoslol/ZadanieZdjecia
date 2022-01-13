69ba57caa86548d23fa89d44a067cb489f5c5027
sztoslol <kubuszeski05@wp.pl>
2022-01-13 12:34:00 +0100

    lol

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,10 +2,34 @@ package com.example.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import androidx.core.widget.ImageViewCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<ImageView>(R.id.zdjecia).setImageResource(R.drawable.adrian1)
+        var zdj = 1
+        findViewById<Button>(R.id.dalej).setOnClickListener {
+            zdj++;
+            if(zdj == 1)
+            {
+                findViewById<ImageView>(R.id.zdjecia).setImageResource(R.drawable.adrian1)
+            }
+            if(zdj == 2)
+            {
+                findViewById<ImageView>(R.id.zdjecia).setImageResource(R.drawable.adrian2)
+            }
+            if(zdj == 3)
+            {
+                findViewById<ImageView>(R.id.zdjecia).setImageResource(R.drawable.adrian3)
+            }
+            if(zdj == 4)
+            {
+                findViewById<ImageView>(R.id.zdjecia).setImageResource(R.drawable.adrian4)
+            }
+        }
     }
 }
