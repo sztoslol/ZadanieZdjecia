a500f0db683838ca29e59d797f88103f10e87323
sztoslol <kubuszeski05@wp.pl>
2022-01-13 13:08:03 +0100

    lol

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.myapplication
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import androidx.core.widget.ImageViewCompat
 
@@ -13,7 +14,6 @@ class MainActivity : AppCompatActivity() {
         findViewById<ImageView>(R.id.zdjecia).setImageResource(R.drawable.adrian1)
         var zdj = 1
         findViewById<Button>(R.id.dalej).setOnClickListener {
-            zdj++;
             if(zdj == 1)
             {
                 findViewById<ImageView>(R.id.zdjecia).setImageResource(R.drawable.adrian1)
@@ -29,7 +29,33 @@ class MainActivity : AppCompatActivity() {
             if(zdj == 4)
             {
                 findViewById<ImageView>(R.id.zdjecia).setImageResource(R.drawable.adrian4)
+                zdj = 0;
             }
+            zdj++
+        }
+        findViewById<Button>(R.id.wroc).setOnClickListener {
+            if(zdj == 1)
+            {
+                findViewById<ImageView>(R.id.zdjecia).setImageResource(R.drawable.adrian1)
+                zdj = 5
+            }
+            if(zdj == 2)
+            {
+                findViewById<ImageView>(R.id.zdjecia).setImageResource(R.drawable.adrian2)
+            }
+            if(zdj == 3)
+            {
+                findViewById<ImageView>(R.id.zdjecia).setImageResource(R.drawable.adrian3)
+            }
+            if(zdj == 4)
+            {
+                findViewById<ImageView>(R.id.zdjecia).setImageResource(R.drawable.adrian4)
+            }
+            zdj--
+        }
+        findViewById<Button>(R.id.pozycjaLewo).setOnClickListener {
+            var rotacja = 0
+            if(findViewById<EditText>(R.id.te))
         }
     }
 }
