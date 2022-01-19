64471a59f7472e9255fa60ba908b2312fc7715bf
sztoslol <kubuszeski05@wp.pl>
2022-01-19 15:33:18 +0100

    Gotowe

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -7,61 +7,69 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.core.widget.ImageViewCompat
-import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        findViewById<ImageView>(R.id.zdjecia).setImageResource(R.drawable.adrian1)
+        findViewById<ImageView>(R.id.obraz).setImageResource(R.drawable.adrian1)
         var zdj = 1
-        var rotacja = 0
-        findViewById<Button>(R.id.dalej).setOnClickListener {
+        findViewById<Button>(R.id.DalejBtn).setOnClickListener {
             if(zdj == 1)
             {
-                findViewById<ImageView>(R.id.zdjecia).setImageResource(R.drawable.adrian1)
+                findViewById<ImageView>(R.id.obraz).setImageResource(R.drawable.adrian1)
             }
             if(zdj == 2)
             {
-                findViewById<ImageView>(R.id.zdjecia).setImageResource(R.drawable.adrian2)
+                findViewById<ImageView>(R.id.obraz).setImageResource(R.drawable.adrian2)
             }
             if(zdj == 3)
             {
-                findViewById<ImageView>(R.id.zdjecia).setImageResource(R.drawable.adrian3)
+                findViewById<ImageView>(R.id.obraz).setImageResource(R.drawable.adrian3)
             }
             if(zdj == 4)
             {
-                findViewById<ImageView>(R.id.zdjecia).setImageResource(R.drawable.adrian4)
+                findViewById<ImageView>(R.id.obraz).setImageResource(R.drawable.adrian4)
                 zdj = 0;
             }
             zdj++
         }
-        findViewById<Button>(R.id.wroc).setOnClickListener {
+        findViewById<Button>(R.id.WrocBtn).setOnClickListener {
             if(zdj == 1)
             {
-                findViewById<ImageView>(R.id.zdjecia).setImageResource(R.drawable.adrian1)
+                findViewById<ImageView>(R.id.obraz).setImageResource(R.drawable.adrian1)
                 zdj = 5
             }
             if(zdj == 2)
             {
-                findViewById<ImageView>(R.id.zdjecia).setImageResource(R.drawable.adrian2)
+                findViewById<ImageView>(R.id.obraz).setImageResource(R.drawable.adrian2)
             }
             if(zdj == 3)
             {
-                findViewById<ImageView>(R.id.zdjecia).setImageResource(R.drawable.adrian3)
+                findViewById<ImageView>(R.id.obraz).setImageResource(R.drawable.adrian3)
             }
             if(zdj == 4)
             {
-                findViewById<ImageView>(R.id.zdjecia).setImageResource(R.drawable.adrian4)
+                findViewById<ImageView>(R.id.obraz).setImageResource(R.drawable.adrian4)
             }
             zdj--
         }
-        findViewById<Button>(R.id.pozycjaLewo).setOnClickListener {
-            rotacja = findViewById<TextView>(R.id.Rotacjatext).toString().toInt()
-                findViewById<ImageView>(R.id.zdjecia).setRotationY(
-                    findViewById<ImageView>(R.id.zdjecia).rotationY + rotacja
-                )
+        findViewById<Button>(R.id.PozLewaBtn).setOnClickListener {
+            var rotacja = "-" + findViewById<TextView>(R.id.Rotacjatext).text
+            findViewById<ImageView>(R.id.obraz).rotationY = rotacja.toString().toFloat() + findViewById<ImageView>(R.id.obraz).rotationY
+        }
+        findViewById<Button>(R.id.PozPrawaBtn).setOnClickListener {
+            var rotacja = findViewById<TextView>(R.id.Rotacjatext).text
+            findViewById<ImageView>(R.id.obraz).rotationY = rotacja.toString().toFloat() + findViewById<ImageView>(R.id.obraz).rotationY
+        }
+        findViewById<Button>(R.id.SkalaBtn).setOnClickListener {
+            var skala = findViewById<TextView>(R.id.SkalaText).text
+            findViewById<ImageView>(R.id.SkalaText).scaleX = skala.toString().toFloat()
+            findViewById<ImageView>(R.id.SkalaText).scaleY = skala.toString().toFloat()
+        }
+        findViewById<Button>(R.id.AlfaBtn).setOnClickListener {
+            var alfa = findViewById<TextView>(R.id.AlfaText).text
+            findViewById<ImageView>(R.id.obraz).alpha = alfa.toString().toFloat()
         }
     }
 }
